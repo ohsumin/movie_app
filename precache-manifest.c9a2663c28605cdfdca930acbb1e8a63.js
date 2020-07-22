3007cc4b227e0b1c4afb14161f43d79cfb09ebd3
ohsumin <otnals25@naver.com>
2020-07-22 21:48:41 +0900

    Updates

---
+++ precache-manifest.c9a2663c28605cdfdca930acbb1e8a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fadc678a15feaa08b5caa69ca6fd27",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4c2b0e047c831bd707fc",
+    "url": "/movie_app/static/css/main.a83c4392.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2b0e047c831bd707fc",
+    "url": "/movie_app/static/js/main.289d9bb8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
